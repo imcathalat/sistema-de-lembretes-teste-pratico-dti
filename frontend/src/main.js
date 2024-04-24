ac31dacd28792f85c8dfd7d0a7cfa98f7ea7ba90
pandora <imcathalat@protonmail.com>
2024-04-24 20:54:12 -0300

    adição do axios no vue.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+import { defaults } from 'cypress/types/lodash'
+
+axios = defaults.baseURL = 'http://127.0.0.1:8000/'
 
 const app = createApp(App)
 
