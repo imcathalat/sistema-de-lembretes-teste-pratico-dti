9b1e2d0e6bde66353d33f2ec179165908960bbeb
pandora <imcathalat@protonmail.com>
2024-04-28 05:40:53 -0300

    implementação da amostragem de lembretes por datas

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -9,13 +9,13 @@ class LembreteSerializer(serializers.ModelSerializer):
         model = Lembrete
         fields = ['lembrete_id','nome', 'data']
 
-    def validate_data(self, value):
-        if not value:
-            raise serializers.ValidationError("O campo 'data' deverá ser preenchido")
+    # def validate_data(self, value):
+    #     if not value:
+    #         raise serializers.ValidationError("O campo 'data' deverá ser preenchido")
 
-        if value >= date.today():
-            raise serializers.ValidationError("A data não pode estar no passado, deve estar no presente ou no futuro")
-        return value
+    #     if value >= date.today():
+    #         raise serializers.ValidationError("A data não pode estar no passado, deve estar no presente ou no futuro")
+    #     return value
 
 
 
--- frontend/src/components/Forms.vue
+++ frontend/src/components/Forms.vue
@@ -1,51 +1,98 @@
 <script setup>
 import { ref, onMounted, computed } from 'vue'
 import axios from 'axios'
-import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 
 
 const name = ref('');
+const date = ref('');
 
 const lembretes = ref([]);
+const lembretesPorData = ref([]);
 
-function postLembrete(){
-    const nome = name.value;
+function agruparLembretesPorData() {
+    const groupedLembretes = {};
 
-    axios.post('/lembretes/', {nome})
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Erro: ', error);
-    })
+    lembretes.value.forEach(lembrete => {
+        // Verifica se já existe uma entrada para a data atual
+        if (!groupedLembretes[lembrete.data]) {
+            // Se não existe, cria um novo array para essa data
+            groupedLembretes[lembrete.data] = [];
+        }
+
+        // Adiciona o lembrete ao array associado à sua data
+        groupedLembretes[lembrete.data].push(lembrete);
+    });
+
+    console.log(groupedLembretes);
+
+    lembretesPorData.value = groupedLembretes;
+
+    console.log(lembretesPorData.value);
+
+    const chaves = Object.keys(lembretesPorData.value);
+
+// Itera sobre as chaves
+chaves.forEach(chave => {
+    // Acessa o array correspondente à chave atual
+    const array = lembretesPorData.value[chave];
+
+    // Imprime a chave
+    console.log(chave + ':');
+
+    // Itera sobre os elementos do array
+    array.forEach(elemento => {
+        // Imprime cada elemento do array
+        console.log('- ' + elemento.nome); // Supondo que 'nome' seja o atributo que você deseja imprimir
+    });
+});
 }
+
 function getLembretes() {
     axios.get('/lembretes/')
-    .then(response => {
-        console.log('data', response.data, response.data.pk)
-        lembretes.value = response.data;
-        console.log(lembretes.value.lembrete_id)
-        console.log('lembretes: ', lembretes.value);
-        return response.data;
-        
-    })
-    .catch(error => {
-        console.log('error', error)
-    })
+        .then(response => {
+            lembretes.value = response.data;
+            return response.data;
+
+        })
+        .then(() => {
+            agruparLembretesPorData();
+        })
+        .catch(error => {
+            console.log('error', error)
+        })
+}
+
+function postLembrete() {
+    const nome = name.value;
+
+    const data = new Date(date.value).toISOString().split('T')[0];
+    console.log(data);
+
+    console.log({ nome, data })
+
+    axios.post('/lembretes/', { nome, data })
+        .then(response => {
+            console.log(response.data);
+            getLembretes()
+            name.value = ''
+            date.value = ''
+        })
+        .catch(error => {
+            console.error('Erro: ', error);
+        })
 }
 
 function excluirLembrete(idLembrete) {
     console.log(idLembrete)
     axios.delete(`/lembretes/${idLembrete}/`)
-    .then(response => {
-        console.log('Lembrete excluído:', idLembrete);
-        // Atualizar a lista de lembretes após a exclusão
-        getLembretes();
-    })
-    .catch(error => {
-        console.error('Erro ao excluir lembrete:', error);
-    })
+        .then(response => {
+            console.log('Lembrete excluído:', idLembrete);
+            getLembretes();
+        })
+        .catch(error => {
+            console.error('Erro ao excluir lembrete:', error);
+        })
 }
 
 onMounted(() => {
@@ -55,93 +102,100 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="lembrete-form">
-        <form v-on:submit.prevent="postLembrete" method="post">
-            <fieldset>
-                <legend>Lembrete</legend>
+    <div class="max-w-lg mx-auto">
+        <div class="shadow-md rounded-md p-6 mb-6 lembrete-form">
+            <form v-on:submit.prevent="postLembrete" method="post">
+                <fieldset>
+                    <legend>Lembrete</legend>
 
-                <label for="">Nome</label>
-                <input type="text" v-model="name" class="input-field" required/>
+                    <label for="">Nome</label>
+                    <input type="text" v-model="name" class="input-field" required />
+
+                    <label for="">Data</label>
+                    <input type="date" v-model="date" class="input-field" required />
 
 
-                <input type="submit" value="Submit"/>
-            </fieldset>
-        </form>
-    </div>
-    <div class="lembretes"
-        v-for="lembrete in lembretes"
-        v-bind:key="lembrete.pk"
-    >
-        {{ lembrete.nome }}
+                    <input type="submit" value="Submit" />
+                </fieldset>
+            </form>
+        </div>
+        <div class="shadow-md rounded-md p-6 lembretes">
+            <ul class="list-[square] text-pink-500">
+                <li v-for="(array, chave) in lembretesPorData" :key="chave">
+                        {{ chave }}
+                    <ul>
+                        <li v-for="lembrete in array" v-bind:key="lembrete.lembrete_id">
+                            {{ lembrete.nome }}
+                            <button @click="excluirLembrete(lembrete.lembrete_id)">Excluir</button>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
 
-        <button @click="excluirLembrete(lembrete.lembrete_id)">Excluir</button>
     </div>
 </template>
 
 <style scoped>
-
-.lembrete-form{
-	max-width: 250px;
-	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+.lembrete-form label {
     display: block;
+    margin-top: 0.6rem;
 }
-.lembrete-form label{
-	display:block;
-	margin-top: 0.6rem;
-}
+
 .lembrete-form {
-	color: #F072A9;
-	font-weight: bold;
-	font-size: 1rem;
-	text-shadow: 1px 1px 1px #fff;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+    display: block;
+    color: #F072A9;
+    font-weight: bold;
+    font-size: 1rem;
+    text-shadow: 1px 1px 1px #fff;
 }
-.lembrete-form fieldset{
-	border-radius: 10px;
-	-webkit-border-radius: 10px;
-	-moz-border-radius: 10px;
-	margin: 0px 0px 10px 0px;
-	border: 1px solid #FFD2D2;
-	padding: 20px;
-	background: #FFF4F4;
-	box-shadow: inset 0px 0px 15px #FFE5E5;
-	-moz-box-shadow: inset 0px 0px 15px #FFE5E5;
-	-webkit-box-shadow: inset 0px 0px 15px #FFE5E5;
+
+.lembrete-form fieldset {
+    border-radius: 10px;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    margin: 0px 0px 10px 0px;
+    border: 1px solid #FFD2D2;
+    padding: 20px;
+    background: #FFF4F4;
+    box-shadow: inset 0px 0px 15px #FFE5E5;
+    -moz-box-shadow: inset 0px 0px 15px #FFE5E5;
+    -webkit-box-shadow: inset 0px 0px 15px #FFE5E5;
 }
-.lembrete-form fieldset legend{
-	color: #F072A9;
-	border-top: 1px solid #FFD2D2;
-	border-left: 1px solid #FFD2D2;
-	border-right: 1px solid #FFD2D2;
-	border-radius: 5px 5px 0px 0px;
-	-webkit-border-radius: 5px 5px 0px 0px;
-	-moz-border-radius: 5px 5px 0px 0px;
-	background: #FFF4F4;
-	padding: 0px 8px 3px 8px;
-	box-shadow: -0px -1px 2px #F1F1F1;
-	-moz-box-shadow:-0px -1px 2px #F1F1F1;
-	-webkit-box-shadow:-0px -1px 2px #F1F1F1;
-	font-weight: normal;
-	font-size: 1.3rem;
+
+.lembrete-form fieldset legend {
+    color: #F072A9;
+    border-top: 1px solid #FFD2D2;
+    border-left: 1px solid #FFD2D2;
+    border-right: 1px solid #FFD2D2;
+    border-radius: 5px 5px 0px 0px;
+    -webkit-border-radius: 5px 5px 0px 0px;
+    -moz-border-radius: 5px 5px 0px 0px;
+    background: #FFF4F4;
+    padding: 0px 8px 3px 8px;
+    box-shadow: -0px -1px 2px #F1F1F1;
+    -moz-box-shadow: -0px -1px 2px #F1F1F1;
+    -webkit-box-shadow: -0px -1px 2px #F1F1F1;
+    font-weight: normal;
+    font-size: 1.3rem;
 }
-.lembrete-form{
-	width:500px;
-	height:100px;
-    padding-top: 0px;
-}
-.lembrete-form input[type=text]{
-	border-radius: 3px;
-	-webkit-border-radius: 3px;
-	-moz-border-radius: 3px;
-	border: 1px solid #FFC2DC;
-	outline: none;
-	color: #F072A9;
-	padding: 6px 30px 6px 12px;
-	box-shadow: inset 1px 1px 4px #FFD5E7;
-	-moz-box-shadow: inset 1px 1px 4px #FFD5E7;
-	-webkit-box-shadow: inset 1px 1px 4px #FFD5E7;
-	background: #FFEFF6;
-	width:100%;
-    display:block;
+
+.lembrete-form input[type=text],
+.lembrete-form input[type=date] {
+    border-radius: 3px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border: 1px solid #FFC2DC;
+    outline: none;
+    color: #F072A9;
+    padding: 6px 30px 6px 12px;
+    box-shadow: inset 1px 1px 4px #FFD5E7;
+    -moz-box-shadow: inset 1px 1px 4px #FFD5E7;
+    -webkit-box-shadow: inset 1px 1px 4px #FFD5E7;
+    background: #FFEFF6;
+    width: 100%;
+    display: block;
 }
 
 .lembrete-form input[type=text]:hover {
@@ -149,26 +203,26 @@ onMounted(() => {
 }
 
 
-.lembrete-form  input[type=submit],
-.lembrete-form  input[type=button]{
-	background: #EB3B88;
-	border: 1px solid #C94A81;
-	padding: 5px 15px;
-	color: #FFCBE2;
-	box-shadow: inset -1px -1px 3px #FF62A7;
-	-moz-box-shadow: inset -1px -1px 3px #FF62A7;
-	-webkit-box-shadow: inset -1px -1px 3px #FF62A7;
-	border-radius: 3px;
-	-webkit-border-radius: 3px;
-	-moz-border-radius: 3px;	
-	font-weight: bold;
+.lembrete-form input[type=submit],
+.lembrete-form input[type=button] {
+    background: #EB3B88;
+    border: 1px solid #C94A81;
+    padding: 5px 15px;
+    color: #FFCBE2;
+    box-shadow: inset -1px -1px 3px #FF62A7;
+    -moz-box-shadow: inset -1px -1px 3px #FF62A7;
+    -webkit-box-shadow: inset -1px -1px 3px #FF62A7;
+    border-radius: 3px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    font-weight: bold;
     margin: 8% auto 0 auto;
 }
 
 
-.required{
-	color:red;
-	font-weight:normal;
+.required {
+    color: red;
+    font-weight: normal;
 }
 
 input ::placeholder {
@@ -205,21 +259,21 @@ input ::placeholder {
 }
 
 .datepicker {
-    width:100%;
+    width: 100%;
     background-color: #FFCBE2;
 }
 
 .datepicker input::placeholder {
     color: #C94A81;
 }
+
 .dp__input {
-  border-radius: 3px;
-  font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+    border-radius: 3px;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
 }
 
 
 .lembretes {
     display: block;
 }
-  
 </style>
