7d2c20476841a88d8db42caa4b4ab22a07fac4ad
pandora <imcathalat@protonmail.com>
2024-04-28 14:45:47 -0300

    implementação de teste unitarios no backend: delete e de validação de datas apenas no futuro e presente

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+
 
 import uuid
 
@@ -7,10 +10,15 @@ from datetime import datetime
 from datetime import date
 
 def validar_data_no_presente_ou_futuro(value):
+    if isinstance(value, str):
+        try:
+            value = datetime.strptime(value, '%Y-%m-%d').date()
+        except ValueError:
+            raise ValidationError(_('Formato de data inválido'))
+    
     if value < date.today():
-        raise ValidationError(
-            _('A data não pode estar no passado, deve estar no presente ou no futuro')
-        )
+        raise ValidationError(_('A data não pode estar no passado, deve estar no presente ou no futuro'))
+
 
 class Lembrete(models.Model):
     lembrete_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
